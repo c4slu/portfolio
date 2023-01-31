3659b339ef1781cf39e8be7abac8536b8b5854eb
c4slu <lucasnascimento.brn@gmail.com>
2023-01-31 13:12:08 -0300

    alter

--- script.js
+++ script.js
@@ -9,3 +9,27 @@
       });
     });
   });
+  var elements = document.getElementsByClassName("slide-element");
+  var delay = 0;
+  
+  function checkVisibility() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var rect = element.getBoundingClientRect();
+      if (rect.top >= 50 && rect.bottom <= window.innerHeight) {
+        element.style.transform = "translateX(0%)";
+        element.style.opacity = 1;
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", checkVisibility);
+  
+  window.addEventListener("load", function() {
+    var loader = document.getElementsByClassName("loader")[0];
+    loader.style.opacity = 0;
+    setTimeout(function() {
+      loader.style.display = "none";
+    }, 1000);
+  });
+  
